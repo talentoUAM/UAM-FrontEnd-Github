dcad00ac0324ab6f5c1544406845a7688223e59e
Fernando Cardona <fernando.cardona@the-lion.com>
2025-06-30 13:56:08 +0200

    mas cambios a ver si funciona transfer correctamente

--- app/transfer/page.jsx
+++ app/transfer/page.jsx
@@ -1,39 +1,53 @@
-//IMPORTS REACT/NEXT DEPENDENCIES:
 'use client';
-//IMPORTS REACT/NEXT DEPENDENCIES:
-//IMPORTS REACT/NEXT DEPENDENCIES:
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
-//IMPORTS EXT DEPENDENCIES:
 import { motion, AnimatePresence } from 'framer-motion';
-//IMPORTS GLOBAL STATES:
 import { useGlobalState } from "@/context/GlobalStateContext";
 import CustomButton from "@/components/buttons/CustomButton";
 import { GoArrowRight } from "react-icons/go";
 import { ClientPageRoot } from "next/dist/client/components/client-page";
 import { TfiDownload } from "react-icons/tfi";
-//IMPORTS HOOKS:
-//IMPORTS VARIANTS:
-//IMPORTS COMPONENTS:
 import LoaderComponentInt from "@/components/extraComponents/LoaderComponentInt";
-//IMPORT ICONS:
-//IMPORTS IMAGES:
-//IMPORTS CSS:
-//IMPORT ENV:
 
 const Transfer = () => {
     const { locale, pageTransferData } = useGlobalState();
-    //  console.log('pageTransferData', pageTransferData );
-     
+    const [imageLoaded, setImageLoaded] = useState(false);
+    const [imageInViewport, setImageInViewport] = useState(false);
+    const imageRef = useRef(null);
     
-    // Validación de datos antes de renderizar
-    if ( !pageTransferData) {
-        return <p>Loading...</p>;
+    useEffect(() => {
+        if (!imageRef.current) return;
+
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setImageInViewport(entry.isIntersecting);
+            },
+            {
+                root: null,
+                rootMargin: '0px',
+                threshold: 0.1
+            }
+        );
+
+        observer.observe(imageRef.current);
+
+        return () => {
+            if (imageRef.current) {
+                observer.unobserve(imageRef.current);
+            }
+        };
+    }, []);
+
+    if (!pageTransferData) {
+        return <LoaderComponentInt />;
     }
-   
+
     const { title, image, introTextContent, inovactionTextContent, conocimientoTextContent, endTextContent } = pageTransferData;
 
+    if (!imageLoaded || !imageInViewport) {
+        return <LoaderComponentInt />;
+    }
 
     return (
         <motion.section
@@ -44,8 +58,8 @@ const Transfer = () => {
                 transition: { delay: 0.3 },
             }}
         >   
-            {/* Trining HERO */}
-            <div className="relative w-full h-[80dvh] flex flex-col justify-end">
+            {/* Training HERO */}
+            <div className="relative w-full h-[80dvh] flex flex-col justify-end" ref={imageRef}>
                 {/* Hero Image */}
                 {image?.url && (
                     <Image
@@ -55,6 +69,7 @@ const Transfer = () => {
                         priority
                         loading="eager"
                         alt="Hero Image"
+                        onLoadingComplete={() => setImageLoaded(true)}
                     />
                 )}
 
@@ -68,7 +83,7 @@ const Transfer = () => {
                             transition={{ duration: 0.8 }}
                             className="mb-6"
                         >
-                            <h1 className="text-[40px] md:text-[35px] lg:text-[40px] xl:text-[55px] text-white  font-bold leading-tight uppercase after:content-[''] after:block after:w-[80px] after:h-[4px] after:bg-accent after:mt-[10px]">
+                            <h1 className="text-[40px] md:text-[35px] lg:text-[40px] xl:text-[55px] text-white font-bold leading-tight uppercase after:content-[''] after:block after:w-[80px] after:h-[4px] after:bg-accent after:mt-[10px]">
                                 {title}
                             </h1>
                         </motion.div>
@@ -76,7 +91,7 @@ const Transfer = () => {
                 </div>
             </div>
 
-            {/* INTRO trainig PAGE */}
+            {/* INTRO training PAGE */}
             <div className="w-full flex flex-col justify-center items-start gap-[20px] md:gap-[40px] p-[10px] sm:p-[20px] md:p-[30px] lg:p-[40px] xl:p-[50px] md:mt-[50px]">
                 {/* Intro Text Section */}
                 <div className="w-full">
@@ -106,13 +121,12 @@ const Transfer = () => {
             {/* Text InCompany */}
             <div className="w-full flex flex-col justify-center items-start gap-[30px] p-[10px] sm:p-[20px] md:p-[30px] lg:p-[40px] xl:p-[50px] ">
                 <div className="w-full flex justify-start items-center">
-                    <h2 className="relative inline-block text-[24px] leading-[28px] md:text-[32px] md:leading-[36px] lg:text-[40px] lg:leading-[48px] font-roboto font-semibold text-primary uppercase after:content-[''] after:block after:w-[80px] after:h-[4px] after:bg-accent after:mt-[10px]  ">
+                    <h2 className="relative inline-block text-[24px] leading-[28px] md:text-[32px] md:leading-[36px] lg:text-[40px] lg:leading-[48px] font-roboto font-semibold text-primary uppercase after:content-[''] after:block after:w-[80px] after:h-[4px] after:bg-accent after:mt-[10px]">
                         {locale === 'es' ? ' Proyectos de Innovación' : 'Innovation Projects'}
                     </h2>
                 </div>
                 <div className="w-full flex flex-col justify-start items-start gap-4">
                     {inovactionTextContent?.map((item, index) => {
-                        // Renderizar párrafos
                         if (item.type === "paragraph") {
                             return (
                                 <p key={`${index}-${item.type}`} className="text-[18px] leading-[22px] text-primary font-roboto">
@@ -137,7 +151,6 @@ const Transfer = () => {
                             );
                         }
 
-                        // Renderizar listas (comprobando tanto listas no ordenadas como ordenadas)
                         if (item.type === "list") {
                             const listType = item.format === "unordered" ? "ul" : "ol";
                             return (
@@ -171,7 +184,7 @@ const Transfer = () => {
                             );
                         }
 
-                        return null; // Asegura que solo se rendericen los tipos 'paragraph' y 'list'
+                        return null;
                     })}
                 </div>
             </div>
@@ -179,13 +192,12 @@ const Transfer = () => {
             {/* Text Cursos Cortos */}
             <div className="w-full flex flex-col justify-center items-start gap-[30px] p-[10px] sm:p-[20px] md:p-[30px] lg:p-[40px] xl:p-[50px]">
                 <div className="w-full flex justify-start items-center">
-                    <h2 className="relative inline-block text-[24px] leading-[28px] md:text-[32px] md:leading-[36px] lg:text-[40px] lg:leading-[48px] font-roboto font-semibold text-primary uppercase after:content-[''] after:block after:w-[80px] after:h-[4px] after:bg-accent after:mt-[10px]  ">
+                    <h2 className="relative inline-block text-[24px] leading-[28px] md:text-[32px] md:leading-[36px] lg:text-[40px] lg:leading-[48px] font-roboto font-semibold text-primary uppercase after:content-[''] after:block after:w-[80px] after:h-[4px] after:bg-accent after:mt-[10px]">
                         {locale === 'es' ? 'Transferencia de Conocimiento' : 'Knowledge Transfer'}
                     </h2>
                 </div>
                 <div className="w-full flex flex-col justify-start items-start gap-4">
                     {conocimientoTextContent?.map((item, index) => {
-                        // Renderizar párrafos
                         if (item.type === "paragraph") {
                             return (
                                 <p key={`${index}-${item.type}`} className="text-[18px] leading-[22px] text-primary font-roboto">
@@ -210,7 +222,6 @@ const Transfer = () => {
                             );
                         }
 
-                        // Renderizar listas (comprobando tanto listas no ordenadas como ordenadas)
                         if (item.type === "list") {
                             const listType = item.format === "unordered" ? "ul" : "ol";
                             return (
@@ -244,42 +255,40 @@ const Transfer = () => {
                             );
                         }
 
-                        return null; // Asegura que solo se rendericen los tipos 'paragraph' y 'list'
+                        return null;
                     })}
                 </div>
             </div>
             
             {/* END TEXT PAGE */}
             {Array.isArray(endTextContent) && endTextContent.length > 0 && (
-            <div className="w-full flex flex-col justify-center items-start gap-[20px] md:gap-[40px] p-[10px] sm:p-[20px] md:p-[30px] lg:p-[40px] xl:p-[50px] md:mt-[50px]">
-                {/* Intro Text Section */}
-                <div className="w-full">
-                <div className="w-full">
-                    {endTextContent.map((item, index) => (
-                    <div key={index} className="mb-6">
-                        {item.type === 'paragraph' && (
-                        <p className="text-[18px] text-primary leading-[24px]">
-                            {item.children.map((child, childIndex) => (
-                            <span
-                                key={childIndex}
-                                className={
-                                child.bold
-                                    ? 'text-[20px] leading-[24px] text-primary font-roboto font-bold'
-                                    : 'text-[20px] leading-[24px] text-primary font-roboto'
-                                }
-                            >
-                                {child.text}
-                            </span>
+                <div className="w-full flex flex-col justify-center items-start gap-[20px] md:gap-[40px] p-[10px] sm:p-[20px] md:p-[30px] lg:p-[40px] xl:p-[50px] md:mt-[50px]">
+                    <div className="w-full">
+                        <div className="w-full">
+                            {endTextContent.map((item, index) => (
+                                <div key={index} className="mb-6">
+                                    {item.type === 'paragraph' && (
+                                        <p className="text-[18px] text-primary leading-[24px]">
+                                            {item.children.map((child, childIndex) => (
+                                                <span
+                                                    key={childIndex}
+                                                    className={
+                                                        child.bold
+                                                            ? 'text-[20px] leading-[24px] text-primary font-roboto font-bold'
+                                                            : 'text-[20px] leading-[24px] text-primary font-roboto'
+                                                    }
+                                                >
+                                                    {child.text}
+                                                </span>
+                                            ))}
+                                        </p>
+                                    )}
+                                </div>
                             ))}
-                        </p>
-                        )}
+                        </div>
                     </div>
-                    ))}
                 </div>
-                </div>
-            </div>
             )}
-
         </motion.section>
     );
 };
--- components/extraComponents/LoaderComponentInt.jsx
+++ components/extraComponents/LoaderComponentInt.jsx
@@ -9,12 +9,12 @@ const Loader = () => {
     const { locale, pageTransferData } = useGlobalState();
     return (
         <div className="fixed inset-0 flex flex-col items-center justify-center gap-8 bg-white z-30">
-            <h1>LOADER COMPONENT INT</h1>
             <Image
                 src="/assets/logos/logo-talento-uam.svg" // Ruta dentro de la carpeta public
                 width={120}
                 height={120}
                 alt="Logo Talento UAM"
+                loading="eager"
             />
             <PropagateLoader size='25' color='#F58634'/>
         </div>
